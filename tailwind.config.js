69c12f251d4d6e2b1745a67def26c1c529b8c6c3
Jenny <fenjenjen@gmail.com>
2022-12-21 17:01:33 +0700

    add some animation and fix it (1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
-module.exports = {
-    content: [],
-    theme: {
-      extend: {},
+tailwind.config = {
+  content: [],
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in-down-text1': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+
+        'fade-in-down-text2': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down1': 'fade-in-down-text1 1s ease-out',
+        'fade-in-down2': 'fade-in-down-text2 1s ease-out',
+        'fade-in-down3': 'fade-in-down 3s ease-out',
+        'fade-in-down4': 'fade-in-down 4s ease-out'
+      }
     },
-    plugins: [],
-  }
+  },
+  variants: {},
+  plugins: [],
+}
