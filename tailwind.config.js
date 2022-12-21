ead75a292c3f415e72a002229c143dc974630e63
Jennyver Seztiani Luxman <fenjenjen@gmail.com>
2022-12-22 00:06:50 +0700

    add some animation and fixed it (2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ tailwind.config = {
   theme: {
     extend: {
       keyframes: {
-        'fade-in-down-text1': {
+        'fade-in-down': {
           '0%': {
             opacity: '0',
             transform: 'translateY(-10px)'
@@ -13,24 +13,10 @@ tailwind.config = {
             transform: 'translateY(0)'
           },
         },
-
-        'fade-in-down-text2': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(-10px)'
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)'
-          },
-        }
       },
       animation: {
-        'fade-in-down1': 'fade-in-down-text1 1s ease-out',
-        'fade-in-down2': 'fade-in-down-text2 1s ease-out',
-        'fade-in-down3': 'fade-in-down 3s ease-out',
-        'fade-in-down4': 'fade-in-down 4s ease-out'
-      }
+        'fade-in-down': 'fade-in-down 1s ease-out'
+      },
     },
   },
   variants: {},
